8ba2295cc0c211cd9545d5e53a4495e878468829
yato <2602231824@qq.com>
2020-07-08 11:50:03 +0800

    观察者模式

--- 设计模式/观察者模式/index.js
+++ 设计模式/观察者模式/index.js
@@ -73,3 +73,66 @@ blogger.recommend(456); //调用发布
 //另外一个对象的订阅
 user.addSubscriber(mm.show);
 user.vote(789); //调用发布
+
+
+// 另一种写法
+// 主题 保存状态，状态变化之后触发所有观察者对象
+class Subject {
+    constructor() {
+      this.state = 0
+      this.observers = []
+    }
+    getState() {
+      return this.state
+    }
+    setState(state) {
+      this.state = state
+      this.notifyAllObservers()
+    }
+    notifyAllObservers() {
+      this.observers.forEach(observer => {
+        observer.update()
+      })
+    }
+    attach(observer) {
+      this.observers.push(observer)
+    }
+  
+  
+    remove(observer){
+      const observerIndex = this.observers.findIndex(value => {
+          return value == observer;
+      })
+      observerIndex >= 0 && this.observers.splice(observerIndex, 1);
+  };
+  }
+  
+  // 观察者
+  class Observer {
+    constructor(name, subject) {
+      this.name = name
+      this.subject = subject
+      this.subject.attach(this)
+    }
+    update() {
+      console.log(`${this.name} update, state: ${this.subject.getState()}`)
+    }
+  
+    remove(){
+      this.subject.remove(this)
+    }
+  }
+  
+  // 测试
+  let s = new Subject()
+  let o1 = new Observer('o1', s)
+  let o2 = new Observer('o2', s)
+  
+  s.setState(12)
+  // o1 update, state: 12
+  // o2 update, state: 12
+  
+  o2.remove()
+  s.setState(11) 
+  // o1 update, state: 11
+  
