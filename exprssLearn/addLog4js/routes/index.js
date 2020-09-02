1536099013078a4af7cd4b93b904c34d1f7322c2
yato <2602231824@qq.com>
2020-09-02 10:58:44 +0800

    test express delay send html

--- exprssLearn/addLog4js/routes/index.js
+++ exprssLearn/addLog4js/routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
 const gmLog = require("../utils/logger")("gm")
-
+const fs = require('fs')
+const path = require('path')
+console.log(path.resolve(__dirname ,'../dist/index.html'))
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
   gmLog.info("test req")
+});
+
+router.get('/test', async function (req, res, next) {
+  const sleep = ms => new Promise(resolve => {
+    setTimeout(()=>{
+      res.sendFile(path.resolve(__dirname ,'../dist/index.html'))
+      resolve()
+    }, ms)
+  });
+
+  await sleep(3000)
+
+  gmLog.info("test Html")
 });
 
 module.exports = router;
